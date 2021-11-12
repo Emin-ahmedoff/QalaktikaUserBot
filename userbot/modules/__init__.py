9c12dda10ce5f865935a50e4379b2bee37ef47a7
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-12 23:27:07 +0400

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - ErdewBey - Midy
+# QalaktikaUserBot - Emin - ehmedoff
 #
 
 """ Tüm modülleri yükleyen init dosyası """
